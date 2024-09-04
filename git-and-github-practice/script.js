22400b0d30564aba4236cefc55dfd2d331737290
Alejandro-Lopez83 <alopma83@gmail.com>
2024-09-04 15:59:54 +0100

    he modificado el script para que te rías

--- git-and-github-practice/script.js
+++ git-and-github-practice/script.js
@@ -9,4 +9,10 @@ Que le dice una gallina deprimida a otra gallina deprimida...
 
 
 
-Necesitamos apoyo ajjajajajajajaj
+Necesitamos apoyo ajjajajajajaja
+
+
+Muchas gracias por este chiste que alegró mi corazón jajaja y de paso 
+te cuento yo otro chiste...
+¿Cómo llaman a la Guardia Civil en Holanda?
+VandeVerde
